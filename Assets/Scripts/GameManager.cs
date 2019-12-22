559030b888ee1f76be2faedead5c8034cea428a1
Thomas Cianfarani <thomas.ci83@gmail.com>
2019-12-22 17:37:23 +0100

    Display aliens grids

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public int aliensGridWidth;
+    public int aliensGridHeight;
+    public float aliensWidthSpacing;
+    public float aliensHeightSpacing;
+
+    public GameObject[] aliensPrefabs;
+    public Transform aliensGridParent;
+
+    private float gridInitX;
+    private float gridInitY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gridInitX = Screen.width - 15f;
+        gridInitY = Screen.height / 3f;
+        StartCoroutine(InitAliens());
+    }
+
+    private IEnumerator InitAliens()
+    {
+        for (int h = 0; h < aliensGridHeight; h++)
+        {
+            for (int w = aliensGridWidth; w > 0; w--)
+            {
+                Vector3 pos = new Vector3(gridInitX + w * aliensWidthSpacing, gridInitY + h * aliensHeightSpacing);
+                Instantiate(aliensPrefabs[Random.Range(0, aliensPrefabs.Length)], pos, Quaternion.identity, aliensGridParent);
+                yield return new WaitForSeconds(.05f);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
