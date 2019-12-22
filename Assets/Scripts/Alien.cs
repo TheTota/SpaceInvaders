b19ee7684c6a9d5d3b28d6fbcb94aaa26ca38986
Thomas Cianfarani <thomas.ci83@gmail.com>
2019-12-22 17:59:11 +0100

    Add grid simple movement (no going down or back and forth)

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Alien : MonoBehaviour
-{    
+{
+
     // Start is called before the first frame update
     void Awake()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("TODO: Here play an animation for the death of da boi");
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
+    // Grid spawn/layout
     public int aliensGridWidth;
     public int aliensGridHeight;
     public float aliensWidthSpacing;
@@ -17,9 +18,16 @@ public class GameManager : MonoBehaviour
     private float gridInitX;
     private float gridInitY;
 
+    // Gameplay
+    public float aliensXMoveDistance = .0001f;
+
+    // Useful infos 
+    private List<Alien> livingAliens;
+
     // Start is called before the first frame update
     void Start()
     {
+        livingAliens = new List<Alien>();
         gridInitX = Screen.width - 15f;
         gridInitY = Screen.height / 3f;
         StartCoroutine(InitAliens());
@@ -32,8 +40,23 @@ public class GameManager : MonoBehaviour
             for (int w = aliensGridWidth; w > 0; w--)
             {
                 Vector3 pos = new Vector3(gridInitX + w * aliensWidthSpacing, gridInitY + h * aliensHeightSpacing);
-                Instantiate(aliensPrefabs[Random.Range(0, aliensPrefabs.Length)], pos, Quaternion.identity, aliensGridParent);
-                yield return new WaitForSeconds(.05f);
+                GameObject spawnedAlien = Instantiate(aliensPrefabs[Random.Range(0, aliensPrefabs.Length)], pos, Quaternion.identity, aliensGridParent);
+                livingAliens.Add(spawnedAlien.GetComponent<Alien>());
+                yield return new WaitForSeconds(.085f);
+            }
+        }
+
+        StartCoroutine(MoveAliens());
+    }
+
+    private IEnumerator MoveAliens()
+    {
+        while (livingAliens.Count != 0)
+        {
+            foreach (Alien alien in livingAliens)
+            {
+                alien.transform.Translate(new Vector3(aliensXMoveDistance, 0f, 0f));
+                yield return new WaitForSeconds(.5f);
             }
         }
     }
